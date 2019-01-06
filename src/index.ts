619c10ca30bbad26e4a07bac03279842c1fb60a7
Mathias Scherer <mathias@adwebster.com>
2019-01-06 03:21:09 +0100

    uses correct port for azure

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ mongoose.connect('mongodb://mongo-vkazu-tool:xCDadoQcctQ4KhGCmWO0biXixZJcA3Q2Ykf
         res.sendFile(path.join(__dirname + '/../public/index.html'))
     })
 
-    server.listen(process.env.port || 8000, () => {
-        console.log('Listening on port: ' + (process.env.port || 8000))
+    server.listen(process.env.port || 8080, () => {
+        console.log('Listening on port: ' + (process.env.port || 8080))
     })
 });
