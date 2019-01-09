b9eae6d9b571e857ee71c3630b137935fca0542e
Mathias Scherer <mathias@adwebster.com>
2019-01-09 17:38:41 +0100

    updates webclient

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32ea3e79311110ea3d2e67643029bc7e.js"
+  "/precache-manifest.8818da6ebab2ba9b668e76b0fcadca01.js"
 );
 
 workbox.clientsClaim();
