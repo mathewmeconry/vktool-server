9f0b5db93fdce91baf6278adf7ef669eca63b449
Mathias Scherer <mathias@adwebster.com>
2019-01-10 14:57:45 +0100

    updates webclient

---
+++ public/precache-manifest.3e85ab456e45a0851f810f7ab6af319b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "714a806ae0f1c93236fc",
+    "url": "/static/css/main.805dac5c.chunk.css"
+  },
+  {
+    "revision": "714a806ae0f1c93236fc",
+    "url": "/static/js/main.714a806a.chunk.js"
+  },
+  {
+    "revision": "75e78e93e70d4d079d1f",
+    "url": "/static/css/1.57d3c56e.chunk.css"
+  },
+  {
+    "revision": "75e78e93e70d4d079d1f",
+    "url": "/static/js/1.75e78e93.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e976595567e0e837fa07f665a35f8c54",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e30b1c810285680aadbe04374c0cca1.js"
+  "/precache-manifest.3e85ab456e45a0851f810f7ab6af319b.js"
 );
 
 workbox.clientsClaim();
