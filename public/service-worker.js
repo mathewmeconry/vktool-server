c6c3c7f1bcf244b8f2fd0925203ce501a5a07bf9
Mathias <mathias@localhost.localdomain>
2019-01-22 22:17:24 +0100

    updates webclient

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.234a222175d3825e09120d458562f583.js"
+  "/precache-manifest.4cb42ddcc98894212b3d9d80f51a94c8.js"
 );
 
 workbox.clientsClaim();
