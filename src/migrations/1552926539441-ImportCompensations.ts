5f8e4014ab7c39b88a2ec5d2db07c825c05293d7
Mathias <scherer.mat@gmail.com>
2019-03-18 18:44:20 +0100

    adds migration for the compensations out of the old system

---
+++ src/migrations/1552926539441-ImportCompensations.ts
@@ -0,0 +1,61 @@
+import { MigrationInterface, QueryRunner, getManager, createConnection, getConnectionOptions, getConnection } from "typeorm";
+import CustomCompensation from "../entities/CustomCompensation";
+import fs from 'fs'
+import Contact from "../entities/Contact";
+import User from "../entities/User";
+
+export class ImportCompensations1552926539441 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await createConnection(await getConnectionOptions())
+        let manager = getManager()
+        let user = await manager.findOneOrFail(User, 2)
+
+        let data = fs.readFileSync('src/migrations/compensations_20190318.json')
+        let parsed = JSON.parse(data.toString())
+        for (let record of parsed.data) {
+            try {
+                let contact = await manager.findOneOrFail(Contact, { bexioId: record.bexioID })
+                if (!contact) throw new Error('Contact not found')
+
+                //@ts-ignore
+                let rec = new CustomCompensation(contact, user, parseFloat(record.total), new Date(record.datum), this.genDescription(record.bemerkung, record.von, record.bis), true)
+                manager.save(CustomCompensation, rec)
+            } catch (err) {
+                console.log(`failed with ID ${record.bexioID}`)
+                console.log(err)
+            }
+        }
+
+        return
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await createConnection(await getConnectionOptions())
+        let manager = getManager()
+
+        let data = fs.readFileSync('src/migrations/compensations_20190318.json')
+        let parsed = JSON.parse(data.toString())
+        for (let record of parsed.data) {
+            let contact = await manager.findOneOrFail(Contact, { bexioId: record.bexioID })
+
+            getConnection()
+                .createQueryBuilder()
+                .delete()
+                .from(CustomCompensation)
+                .where('memberId = :contact', { contact: contact.id })
+                .andWhere('amount = :amount', { amount: parseFloat(record.total) })
+                .andWhere('date = :date', { date: record.datum })
+                .andWhere('description = :desc', {desc: this.genDescription(record.bemerkung, record.von, record.bis)})
+                .execute()
+        }
+
+        return
+    }
+
+    private genDescription(bemerkung: string, von?: Date, bis?: Date): string {
+        if (von && bis) return `${bemerkung} (${(new Date(von)).toTimeString()} - ${(new Date(bis)).toTimeString()})`
+
+        return bemerkung
+    }
+}
