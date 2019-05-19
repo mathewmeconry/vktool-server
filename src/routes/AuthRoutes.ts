562c2dc31fea7c6bf6d5865c11f9a4ade76d8d48
Mathias Scherer <scherer.mat@gmail.com>
2019-05-19 13:58:46 +0200

    update authroutes

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -10,7 +10,7 @@ export default function AuthRoutes(app: Express.Application) {
     app.get('/api/auth/azure/callback', AuthController.callbackAzure)
     app.get('/api/logout', AuthController.logout)
 
-    // route for mocked login
+    // route for mocked login 
     if (process.env.TESTING) app.get('/api/auth/mock-admin', passport.authenticate('mock-admin'))
     if (process.env.TESTING) app.get('/api/auth/mock', passport.authenticate('mock-nonadmin'))
     if (process.env.TESTING) app.get('/check/authservice', AuthService.checkAuthorization([AuthRoles.MEMBERS_READ]), (req, res) => { res.status(200); res.send('') })
