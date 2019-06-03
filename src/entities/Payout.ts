6a89f22f2362253e6e8a74fe09e2c7da6de18f6a
Mathias Scherer <scherer.mat@gmail.com>
2019-06-03 13:13:30 +0200

    adds the calculation of the total to payouts

--- src/entities/Payout.ts
+++ src/entities/Payout.ts
@@ -1,5 +1,5 @@
 import Base from "./Base";
-import { Entity, Column, OneToMany, JoinColumn, ManyToOne, getManager, Not, LessThan } from "typeorm";
+import { Entity, Column, OneToMany, JoinColumn, ManyToOne, getManager, Not, LessThan, AfterLoad } from "typeorm";
 import Compensation from "./Compensation";
 import User from "./User";
 import { IsDate } from "class-validator";
@@ -19,6 +19,8 @@ export default class Payout extends Base<Payout> {
 
     @Column('date')
     public from: Date
+
+    public total: number = 0
 
     constructor(until: Date, from = new Date('1970-01-01')) {
         super()
@@ -41,5 +43,12 @@ export default class Payout extends Base<Payout> {
             .andWhere('deletedAt is NULL')
 
         return query.execute()
+    }
+
+    @AfterLoad()
+    private calculateTotal() {
+        for (let compensation of this.compensations) {
+            this.total = this.total + compensation.amount
+        }
     }
 }
