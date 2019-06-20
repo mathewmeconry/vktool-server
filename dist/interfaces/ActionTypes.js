bcf30048ef63d992a7b1b32a77238dce6be8eeb1
Mathias Scherer <scherer.mat@gmail.com>
2019-06-20 09:29:23 +0200

    removes finally the dist folder

--- dist/interfaces/ActionTypes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DataActions = {
-    FETCH_CONTACTS: 'fetch_contacts'
-};
-//# sourceMappingURL=ActionTypes.js.map
