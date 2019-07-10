55a619a72bdfe38f63c0ae7d65a8c4bfa899090b
Mathias Scherer <scherer.mat@gmail.com>
2019-07-10 18:53:39 +0200

    updates webclient

---
+++ public/precache-manifest.0eb039d5640471b2c33207ac51376c13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "476afc811cfcdda036f7",
+    "url": "/static/css/main.c57bdbc7.chunk.css"
+  },
+  {
+    "revision": "476afc811cfcdda036f7",
+    "url": "/static/js/main.476afc81.chunk.js"
+  },
+  {
+    "revision": "a2b195253e4b4cea045a",
+    "url": "/static/css/1.9f2878e5.chunk.css"
+  },
+  {
+    "revision": "a2b195253e4b4cea045a",
+    "url": "/static/js/1.a2b19525.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "46f55baf5a31b1358f6489d0d898a510",
+    "url": "/index.html"
+  }
+];
