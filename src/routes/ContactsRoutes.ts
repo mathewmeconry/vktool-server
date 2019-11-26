fcf7cce16288060a42d7c0c091aef23118fca6ec
Mathias Scherer <scherer.mat@gmail.com>
2019-11-26 13:55:13 +0100

    generates memberlist server side refactors pdf generation and introduces memberService

--- src/routes/ContactsRoutes.ts
+++ src/routes/ContactsRoutes.ts
@@ -10,6 +10,7 @@ export default function ContactsRoutes(app: Express.Application) {
     app.get('/api/members', AuthService.checkAuthorization([AuthRoles.MEMBERS_READ, AuthRoles.BILLINGREPORTS_CREATE]), ContactsController.getMembers)
     app.post('/api/members', AuthService.checkAuthorization([AuthRoles.CONTACTS_EDIT]), ContactsController.postContact)
 
+    app.get('/api/members/pdf', AuthService.checkAuthorization([AuthRoles.AUTHENTICATED]), ContactsController.getMemberListPdf)
 
     app.get('/api/ranks', AuthService.checkAuthorization([AuthRoles.RANKS_READ]), ContactsController.getRanks)
 }
