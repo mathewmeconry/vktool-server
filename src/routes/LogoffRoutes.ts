e5cc8a294e461952d8e11879e6b463c6bc3288bd
Mathias Scherer <mathias.scherer@tamedia.ch>
2020-04-25 17:14:36 +0200

    changes permissions for some endpoints

--- src/routes/LogoffRoutes.ts
+++ src/routes/LogoffRoutes.ts
@@ -4,7 +4,7 @@ import { AuthRoles } from "../interfaces/AuthRoles"
 import LogoffController from "../controllers/LogoffController"
 
 export default function LogoffRoutes(app: Express.Router) {
-    app.get('/logoffs', AuthService.checkAuthorization([AuthRoles.AUTHENTICATED]), LogoffController.getAll)
+    app.get('/logoffs', AuthService.checkAuthorization([AuthRoles.LOGOFFS_READ]), LogoffController.getAll)
 
     app.put('/logoffs/add', AuthService.checkAuthorization([AuthRoles.LOGOFFS_CREATE]), LogoffController.add)
     app.put('/logoffs/add/bulk', AuthService.checkAuthorization([AuthRoles.LOGOFFS_CREATE]), LogoffController.addBulk)
