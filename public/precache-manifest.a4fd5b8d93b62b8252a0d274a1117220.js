b1a6492bc3403e5be8a4ca86fcf962ca65c2e2cc
Mathias Scherer <mathias.scherer@tamedia.ch>
2020-06-02 09:06:54 +0200

    updates webclient

---
+++ public/precache-manifest.a4fd5b8d93b62b8252a0d274a1117220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65c26c62aa662cc6acef",
+    "url": "/webapp/static/js/runtime~main.65c26c62.js"
+  },
+  {
+    "revision": "c4fd958ca0f72cfc94c5",
+    "url": "/webapp/static/js/main.c4fd958c.chunk.js"
+  },
+  {
+    "revision": "68346ce4ca8d0f39068b",
+    "url": "/webapp/static/js/1.68346ce4.chunk.js"
+  },
+  {
+    "revision": "c4fd958ca0f72cfc94c5",
+    "url": "/webapp/static/css/main.4c1b6b28.chunk.css"
+  },
+  {
+    "revision": "68346ce4ca8d0f39068b",
+    "url": "/webapp/static/css/1.46050b57.chunk.css"
+  },
+  {
+    "revision": "62221d1e5391695d64e73294bbde501b",
+    "url": "/webapp/index.html"
+  }
+];
