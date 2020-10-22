68df33cd728d6406f9d14faa53beaef143e1cd80
Mathias Scherer <mathias.scherer@tamedia.ch>
2020-10-22 22:02:47 +0200

    corrects warehouse routes to meet the schema

--- src/routes/WarehouseRoutes.ts
+++ src/routes/WarehouseRoutes.ts
@@ -4,6 +4,6 @@ import { AuthRoles } from '../interfaces/AuthRoles';
 import AuthService from '../services/AuthService';
 
 export default function WarehouseRoutes(app: Express.Router) {
-    app.get('/warehouse/:id/report', AuthService.checkAuthorization([AuthRoles.WAREHOUSE_READ]), WarehouseController.getReport)
-    app.get('/warehouses/report', AuthService.checkAuthorization([AuthRoles.WAREHOUSE_READ]), WarehouseController.getTotalReport)
+    app.get('/warehouse/:id/report/pdf', AuthService.checkAuthorization([AuthRoles.WAREHOUSE_READ]), WarehouseController.getReport)
+    app.get('/warehouses/report/pdf', AuthService.checkAuthorization([AuthRoles.WAREHOUSE_READ]), WarehouseController.getTotalReport)
 }
