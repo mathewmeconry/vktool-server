23e4ebb923f911030f1898fca6ff60b1f2a4b3b8
Mathias Scherer <scherer.mat@gmail.com>
2022-06-20 22:24:43 +0200

    run cron every 3 hours

--- src/services/BexioService.ts
+++ src/services/BexioService.ts
@@ -183,12 +183,12 @@ export namespace BexioService {
 	}
 
 	export async function regsiterCronJobs() {
-		await CronJobs.register(BexioService.syncContactGroups, 30 * 60 * 1000);
-		await CronJobs.register(BexioService.syncContactTypes, 30 * 60 * 1000);
-		await CronJobs.register(BexioService.syncContacts, 30 * 60 * 1000);
-		await CronJobs.register(BexioService.syncOrders, 30 * 60 * 1000);
-		await CronJobs.register(BexioService.syncBillStatuses, 30 * 60 * 1000);
-		await CronJobs.register(BexioService.syncProducts, 30 * 60 * 1000);
+		await CronJobs.register(BexioService.syncContactGroups, 3 * 60 * 60 * 1000);
+		await CronJobs.register(BexioService.syncContactTypes, 3 * 60 * 60 * 1000);
+		await CronJobs.register(BexioService.syncContacts, 3 * 60 * 60 * 1000);
+		await CronJobs.register(BexioService.syncOrders, 3 * 60 * 60 * 1000);
+		await CronJobs.register(BexioService.syncBillStatuses, 3 * 60 * 60 * 1000);
+		await CronJobs.register(BexioService.syncProducts, 3 * 60 * 60 * 1000);
 	}
 
 	/**
