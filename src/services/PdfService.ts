d613fa8476bb6dd8bdc6829d74c556ff0d47983b
Mathias Scherer <scherer.mat@gmail.com>
2022-11-23 11:02:10 +0100

    fix: time formatting in billingReport receipt

--- src/services/PdfService.ts
+++ src/services/PdfService.ts
@@ -283,11 +283,11 @@ export default class PdfService {
 			[index: string]: { from: string; until: string; charge: string; amount: number };
 		} = {};
 		for (const comp of billingReport.compensations) {
-			const compReduceId = `${comp.from.getDate()}_${comp.until.getDate()}_${comp.charge}`;
+			const compReduceId = `${comp.from.getTime()}_${comp.until.getTime()}_${comp.charge}`;
 			if (!combinedComps.hasOwnProperty(compReduceId)) {
 				combinedComps[compReduceId] = {
-					from: moment(comp.from).format('hh:mm'),
-					until: moment(comp.until).format('hh:mm'),
+					from: moment(comp.from).format('HH:mm'),
+					until: moment(comp.until).format('HH:mm'),
 					charge: comp.charge ? 'Ja' : 'Nein',
 					amount: 0,
 				};
