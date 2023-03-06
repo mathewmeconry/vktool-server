fbcd28cb3050a9c5bbb8647ea07fe9f9da0d8b72
Mathias Scherer <scherer.mat@gmail.com>
2023-03-06 21:19:25 +0100

    feat: add member csv export

--- src/controllers/ContactsController.ts
+++ src/controllers/ContactsController.ts
@@ -1,19 +1,56 @@
-import * as Express from 'express'
-import ContactService from '../services/ContactService'
-import moment from 'moment'
-import PdfService from '../services/PdfService'
+import * as Express from 'express';
+import ContactService from '../services/ContactService';
+import moment from 'moment';
+import PdfService from '../services/PdfService';
 
 export default class ContactsController {
-    public static async getMemberListPdf(req: Express.Request, res: Express.Response): Promise<void> {
-        let members = await ContactService.getActiveMembers(
-            false
-        )
+	public static async getMemberListPdf(req: Express.Request, res: Express.Response): Promise<void> {
+		let members = await ContactService.getActiveMembers(false);
 
-        res.contentType('application/pdf')
-        res.setHeader(
-            'Content-Disposition',
-            `inline; filename=Mitgliederliste ${moment(new Date()).format('DD-MM-YYYY')}.pdf`
-        )
-        res.send(await PdfService.generateMemberList(members))
-    }
+		res.contentType('application/pdf');
+		res.setHeader(
+			'Content-Disposition',
+			`inline; filename=Mitgliederliste ${moment(new Date()).format('DD-MM-YYYY')}.pdf`
+		);
+		res.send(await PdfService.generateMemberList(members));
+	}
+
+	public static async getMemberListCsv(req: Express.Request, res: Express.Response): Promise<void> {
+		let members = await ContactService.getActiveMembers(false);
+
+		const csv = [
+			[
+				'Vorname',
+				'Nachname',
+				'Grad',
+				'Funktionen',
+				'Adresse',
+				'Abholpunk',
+				'Festnetz',
+				'Mobile',
+				'Festnetz',
+			].join(';'),
+		];
+		for (const member of members) {
+			csv.push(
+				[
+					member.firstname,
+					member.lastname,
+					member.rank || '',
+					(member.functions || []).join(','),
+					member.address,
+					member.extension?.collectionPoint?.address || '',
+					member.phoneFixed || '',
+					member.phoneMobile || '',
+				].join(';').replace(/\n/g, '').replace(/\r/g, '')
+			);
+		}
+
+		res.contentType('text/csv');
+		res.setHeader(
+			'Content-Disposition',
+			`inline; filename=Mitgliederliste ${moment(new Date()).format('DD-MM-YYYY')}.csv`
+		);
+		res.send(csv.join('\n'));
+	}
 }
